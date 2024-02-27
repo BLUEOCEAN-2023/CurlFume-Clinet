49f5228b1bf53e8f33f1c1d59abafb67c941715b
misung <voielactee7@naver.com>
2024-02-27 23:21:58 +0900

    :sparkles: [feat] 결과화면 피라미드 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Question1 from "./components/AskScreen/Question1";
 import Question2 from "./components/AskScreen/Question2";
 import Question3 from "./components/AskScreen/Question3";
 import Question4 from "./components/AskScreen/Question4";
+import ResultScreen from "./components/ResultScreen/ResultScreen";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/perfume2" element={<Question2 />} />
         <Route path="/perfume3" element={<Question3 />} />
         <Route path="/perfume4" element={<Question4 />} />
+        <Route path="/result" element={<ResultScreen />} />
       </Routes>
     </Router>
   );
