09378421eb36c12693fb9044d5568567aa97c3da
misung <voielactee7@naver.com>
2024-02-28 16:10:13 +0900

    :art: [fix] 질문화면 클래스명 변경 및 css 코드 리팩토링, 배경색 변경

--- src/components/AskScreen/Question1.js
+++ src/components/AskScreen/Question1.js
@@ -11,36 +11,35 @@ function Question1() {
 
   const handleSelect = (index) => {
     setSelected(index);
+    console.log("Selected button ID:", buttons[index].id);
     navigate("/perfume2");
   };
 
   const buttons = [
-    { id: 1, text: "첫번째 대답" },
-    { id: 2, text: "두번째 대답" },
-    { id: 3, text: "세번째 대답" },
-    { id: 4, text: "네번째 대답" },
+    { id: 1, text: "플로럴" },
+    { id: 2, text: "머스크" },
   ];
 
   return (
     <div className={styles["questionScreen"]}>
-      <div className={styles["progress"]}>
+      <div className={styles["progressimg"]}>
         <img src="../../img/AskScreen/perfumeBottle01.png" alt="진행도" />
       </div>
-      <div className={styles["question"]}>
-        <p className={styles["ask"]}>첫번째 지이이이일무우우운</p>
+      <div className={styles["questionbox"]}>
+        <p className={styles["ask"]}>플로럴 VS 머스크</p>
+        {buttons.map((button, index) => (
+          <div className={styles["answer"]} key={button.id}>
+            <button
+              className={`${styles["answerbtn"]} ${
+                selected === index ? styles["selected"] : ""
+              }`}
+              onClick={() => handleSelect(index)}
+            >
+              {button.text}
+            </button>
+          </div>
+        ))}
       </div>
-      {buttons.map((button, index) => (
-        <div className={styles["answer"]} key={button.id}>
-          <button
-            className={`${styles["answerbtn"]} ${
-              selected === index ? styles["selected"] : ""
-            }`}
-            onClick={() => handleSelect(index)}
-          >
-            {button.text}
-          </button>
-        </div>
-      ))}
     </div>
   );
 }
--- src/components/AskScreen/Question3.js
+++ src/components/AskScreen/Question3.js
@@ -15,32 +15,30 @@ function Question3() {
   };
 
   const buttons = [
-    { id: 1, text: "첫번째 대답" },
-    { id: 2, text: "두번째 대답" },
-    { id: 3, text: "세번째 대답" },
-    { id: 4, text: "네번째 대답" },
+    { id: 1, text: "플로럴" },
+    { id: 2, text: "프루티" },
   ];
 
   return (
     <div className={styles["questionScreen"]}>
-      <div className={styles["progress"]}>
+      <div className={styles["progressimg"]}>
         <img src="../../img/AskScreen/perfumeBottle03.png" alt="진행도" />
       </div>
-      <div className={styles["question"]}>
-        <p className={styles["ask"]}>세번째 지이이이일무우우운</p>
+      <div className={styles["questionbox"]}>
+        <p className={styles["ask"]}>플로럴 VS 프루티</p>
+        {buttons.map((button, index) => (
+          <div className={styles["answer"]} key={button.id}>
+            <button
+              className={`${styles["answerbtn"]} ${
+                selected === index ? styles["selected"] : ""
+              }`}
+              onClick={() => handleSelect(index)}
+            >
+              {button.text}
+            </button>
+          </div>
+        ))}
       </div>
-      {buttons.map((button, index) => (
-        <div className={styles["answer"]} key={button.id}>
-          <button
-            className={`${styles["answerbtn"]} ${
-              selected === index ? styles["selected"] : ""
-            }`}
-            onClick={() => handleSelect(index)}
-          >
-            {button.text}
-          </button>
-        </div>
-      ))}
     </div>
   );
 }
