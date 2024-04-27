12562093df757ad640dbcd60abd5d0bf63642601
misung <voielactee7@naver.com>
2024-04-27 21:35:28 +0900

    :zap: scss 환경설정

--- curlfume-clinet/src/components/StartScreen/StartScreen.jsx
+++ curlfume-clinet/src/components/StartScreen/StartScreen.jsx
@@ -1,1 +1,15 @@
 // 시작 컴포넌트
+
+import React from "react";
+import "../../css/StartScreen/StartScreen.scss";
+
+const StartScreen = () => {
+  return (
+    <>
+      <p>테스트</p>
+      <p>테스트 글자</p>
+    </>
+  );
+};
+
+export default StartScreen;
