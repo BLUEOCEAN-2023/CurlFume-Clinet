0d8a22bbc0faaabcd8d01ba9429e552c8e89aae7
misung <voielactee7@naver.com>
2024-05-21 22:57:01 +0900

    :art: 선택화면 scss 추가

--- curlfume-clinet/src/components/QuestionScreen/LastQuestion.jsx
+++ curlfume-clinet/src/components/QuestionScreen/LastQuestion.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams, useNavigate, Outlet } from "react-router-dom";
 import LastQuestionData from "./LastQuestionList.json";
+import "../../css/QuestionScreen/QuestionScreen.scss";
 
 const LastQuestion = () => {
   const { baseResult, type } = useParams();
@@ -22,19 +23,23 @@ const LastQuestion = () => {
   return (
     <>
       <div>
-        <div>{currentData.question_no}</div>
-        <div>{currentData.question}</div>
-        <div className="answer_container">
-          {currentData.answers.map((answer, index) => (
-            <div className="answer_content" key={index}>
-              <button
-                data-result={answer.type} // type을 사용하여 데이터를 렌더링합니다.
-                onClick={() => handleButtonClick(answer.type)}
-              >
-                {answer.content}
-              </button>
-            </div>
-          ))}
+        <div className="question_container">
+          <div className="question_title_container">
+            <div className="question_no">{currentData.question_no}</div>
+            <div className="question_title">{currentData.question}</div>
+          </div>
+          <div className="answer_container">
+            {currentData.answers.map((answer, index) => (
+              <div className="answer_content" key={index}>
+                <button
+                  data-result={answer.type} // type을 사용하여 데이터를 렌더링합니다.
+                  onClick={() => handleButtonClick(answer.type)}
+                >
+                  {answer.content}
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <Outlet /> {/* 중첩된 라우트를 렌더링하는 부분 */}
--- curlfume-clinet/src/components/QuestionScreen/MiddleQuestion.jsx
+++ curlfume-clinet/src/components/QuestionScreen/MiddleQuestion.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams, useNavigate, Outlet } from "react-router-dom";
 import MiddleQuestionData from "./MiddleQuestionList.json";
+import "../../css/QuestionScreen/QuestionScreen.scss";
 
 const MiddleQuestion = () => {
   const { baseResult } = useParams();
@@ -25,19 +26,23 @@ const MiddleQuestion = () => {
   return (
     <>
       <div>
-        <div>{currentData.question_no}</div>
-        <div>{currentData.question}</div>
-        <div className="answer_container">
-          {currentData.answers.map((answer, index) => (
-            <div className="answer_content" key={index}>
-              <button
-                data-type={answer.type}
-                onClick={() => handleButtonClick(answer.type)}
-              >
-                {answer.content}
-              </button>
-            </div>
-          ))}
+        <div className="question_container">
+          <div className="question_title_container">
+            <div className="question_no">{currentData.question_no}</div>
+            <div className="question_title">{currentData.question}</div>
+          </div>
+          <div className="answer_container">
+            {currentData.answers.map((answer, index) => (
+              <div className="answer_content" key={index}>
+                <button
+                  data-type={answer.type}
+                  onClick={() => handleButtonClick(answer.type)}
+                >
+                  {answer.content}
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <Outlet /> {/* 중첩된 라우트를 렌더링하는 부분 */}
