a42de50b23deb5b719ec7c4f40df2fc9f9695ff3
misung <voielactee7@naver.com>
2024-05-22 22:28:13 +0900

    :sparkles: 404 에러 페이지 구현

--- curlfume-clinet/src/App.jsx
+++ curlfume-clinet/src/App.jsx
@@ -3,6 +3,7 @@ import StartScreen from "./components/StartScreen/StartScreen";
 import Question from "./components/QuestionScreen/Question";
 import MiddleQuestion from "./components/QuestionScreen/MiddleQuestion";
 import LastQuestion from "./components/QuestionScreen/LastQuestion";
+import NotFoundPage from "./components/NotFoundPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/perfume" element={<Question />} />
         <Route path="/perfume/:baseResult" element={<MiddleQuestion />} />
         <Route path="/perfume/:baseResult/:type" element={<LastQuestion />} />
+        <Route path="/404" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
