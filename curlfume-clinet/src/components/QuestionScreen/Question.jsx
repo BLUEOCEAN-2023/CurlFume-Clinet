b4ebb8b10d1a1f7363b7b37f3194626c8698d009
Hanrabbit06 <s2218@e-mirim.hs.kr>
2024-06-10 08:22:06 +0900

    :lipstick: 비율 그래프 생성

--- curlfume-clinet/src/components/QuestionScreen/Question.jsx
+++ curlfume-clinet/src/components/QuestionScreen/Question.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import Questions from "./QuestionList.json";
 import { useNavigate } from "react-router-dom";
@@ -7,6 +8,7 @@ const Question = () => {
   const navigate = useNavigate();
   const [currentId, setCurrentId] = useState(1);
   const [perfume, setPerfume] = useState([]);
+  // eslint-disable-next-line no-unused-vars
   const [baseResult, setBaseResult] = useState("");
   const TOTAL_PAGES = 6;
 
