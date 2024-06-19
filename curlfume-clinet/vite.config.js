75d8f7f8412872289e84749f3144b299ba441b6c
misung <voielactee7@naver.com>
2024-06-19 10:59:00 +0900

    :sparkles: 결과화면 로딩 스피너 구현

--- curlfume-clinet/vite.config.js
+++ curlfume-clinet/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000/CurlFume-Clinet/curlfume-clinet/public/api/fetch_TypeData.php', // PHP 서버의 URL
+      "/api": {
+        target:
+          "http://localhost:8000/CurlFume-Clinet/curlfume-clinet/public/api/fetch_TypeData.php", // PHP 서버의 URL
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/public/api/'),
+        rewrite: (path) => path.replace(/^\/api/, "/public/api/"),
       },
     },
   },
